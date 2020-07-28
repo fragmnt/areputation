791174232d24dc1774bc327e8a4a2c9e11e37104
Fragmnt X <fragmnt@x.com>
2020-07-28 07:02:37 -0400

    kudos smart contract

---
+++ contracts/src/Kudos.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Apache 2.0
+
+pragma solidity ^0.6.9;
+
+contract Kudos {
+
+    uint256 vote;
+    address public contributor;
+    address public organization;
+
+    struct Task {
+        uint256 task_id;
+        address doer;
+        bytes24 description;
+        bytes16 name;
+        uint32 timestamp;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor(address _contributor) public {
+        organization = msg.sender;
+        contributor = _contributor;
+    }
+
+    function submit(uint256 task_id) public view returns (uint) {
+        return tasks[task_id].task_id;
+    }
+}
